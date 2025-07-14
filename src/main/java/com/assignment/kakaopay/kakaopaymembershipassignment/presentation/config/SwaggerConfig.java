6411486cce0855a47506ffc83e27fab20ef2ba85
juneunbae <dmsqo1403@naver.com>
2025-07-14 21:27:41 +0900

    feat: Swagger UI 연동

---
+++ src/main/java/com/assignment/kakaopay/kakaopaymembershipassignment/presentation/config/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package com.assignment.kakaopay.kakaopaymembershipassignment.presentation.config;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+
+import io.swagger.v3.core.jackson.ModelResolver;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI()
+			.components(new Components())
+			.info(apiInfo());
+	}
+
+	private Info apiInfo() {
+		return new Info()
+			.title("Kakaopay Membership Assignment")
+			.description("Kakaopay Membership Assignment API")
+			.version("1.0");
+	}
+
+	@Bean
+	public GroupedOpenApi v1Api() {
+		return GroupedOpenApi.builder()
+			.group("api v1")
+			.pathsToMatch("/api/**")
+			.build();
+	}
+
+	@Bean
+	public ModelResolver modelResolver(ObjectMapper objectMapper) {
+		return new ModelResolver(objectMapper.setPropertyNamingStrategy(PropertyNamingStrategies.LOWER_CAMEL_CASE));
+	}
+}
