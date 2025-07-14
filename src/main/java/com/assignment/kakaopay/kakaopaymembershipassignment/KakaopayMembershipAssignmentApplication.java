5a9d57c1dc3421b0bd50fd538d1724e25e4d0906
juneunbae <dmsqo1403@naver.com>
2025-07-14 18:43:03 +0900

    first commit

---
+++ src/main/java/com/assignment/kakaopay/kakaopaymembershipassignment/KakaopayMembershipAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.kakaopay.kakaopaymembershipassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaopayMembershipAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaopayMembershipAssignmentApplication.class, args);
+	}
+
+}
