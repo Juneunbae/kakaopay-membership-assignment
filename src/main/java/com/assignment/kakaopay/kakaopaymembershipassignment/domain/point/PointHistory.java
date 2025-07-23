b538efb8e4f270b50d2bc966a4bdfbafb117d436
juneunbae <dmsqo1403@naver.com>
2025-07-23 17:17:36 +0900

    fix: PointHistory 엔티티 수정
    컬럼 수정
    - Store store -> Long storeId

--- src/main/java/com/assignment/kakaopay/kakaopaymembershipassignment/domain/point/PointHistory.java
+++ src/main/java/com/assignment/kakaopay/kakaopaymembershipassignment/domain/point/PointHistory.java
@@ -7,18 +7,14 @@ import org.hibernate.annotations.DynamicUpdate;
 import org.springframework.data.annotation.CreatedDate;
 
 import com.assignment.kakaopay.kakaopaymembershipassignment.domain.category.Category;
-import com.assignment.kakaopay.kakaopaymembershipassignment.domain.store.Store;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -55,9 +51,7 @@ public class PointHistory {
 	)
 	private Action action;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "store_id", nullable = false)
-	private Store store;
+	private Long storeId;
 
 	@CreatedDate
 	@Column(
