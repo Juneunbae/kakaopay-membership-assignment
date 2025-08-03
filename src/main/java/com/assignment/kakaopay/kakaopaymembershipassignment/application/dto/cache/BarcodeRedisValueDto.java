7499ed35345f6270ab26f09e76f82ba3d696953f
juneunbae <dmsqo1403@naver.com>
2025-08-03 15:31:00 +0900

    fix: BarcodeRedisValueDto.java 수정
    BarcodeRedisValueDto.java
    - 기존 필드(barcode, userId, categoryPoints) -> 수정 필드(barcode, userId)

--- src/main/java/com/assignment/kakaopay/kakaopaymembershipassignment/application/dto/cache/BarcodeRedisValueDto.java
+++ src/main/java/com/assignment/kakaopay/kakaopaymembershipassignment/application/dto/cache/BarcodeRedisValueDto.java
@@ -1,10 +1,7 @@
 package com.assignment.kakaopay.kakaopaymembershipassignment.application.dto.cache;
 
-import java.util.Map;
-
 public record BarcodeRedisValueDto(
 	String barcode,
-	Long userId,
-	Map<String, Integer> categoryPoints
+	Long userId
 ) {
 }
